8b89a709babadfb2f27a4dbfdea6986e369e526b
Aengus MacKenzie <aengus.2000@gmail.com>
2026-05-25 14:18:55 -0400

    Track article page views with session

--- server/app.py
+++ server/app.py
@@ -27,8 +27,25 @@ def index_articles():
 
 @app.route('/articles/<int:id>')
 def show_article(id):
-    pass
+    if 'page_views' not in session:
+        session['page_views'] = 0
 
+    session['page_views'] += 1
+
+    if session['page_views'] > 3:
+        return {'message': 'Maximum pageview limit reached'}, 401
+
+    article = Article.query.filter(Article.id == id).first()
+
+    return {
+        "id": article.id,
+        "author": article.author,
+        "title": article.title,
+        "content": article.content,
+        "preview": article.preview,
+        "minutes_to_read": article.minutes_to_read,
+        "date": article.date,
+    }, 200
 
 if __name__ == '__main__':
     app.run(port=5555)
